27547100a52f0c449dd48bb05fcb818cc5b976a3
forever208 <ningmang666@gmail.com>
2024-01-18 14:34:52 +0100

    MAE BP4D AU baseline

---
+++ AU/AU_json_process.py
@@ -0,0 +1,83 @@
+import base64
+import csv
+from PIL import Image
+import os
+import cv2
+import json
+import random
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+TEST_SUBJECTS = ["F002", "F004", "F018", "F019", "F023", "M006", "M007", "M010", ]  # BP4D F1=0.661
+
+BP4D_SUBJECTS = ['F015', 'M005', 'F017', 'M013', 'M009', 'M011', 'M016', 'F004', 'M007', 'F002', 'F008', 'M014', 'F007', 'F014', 'M006', 'M002', 'F016', 'F018', 'F003', 'F009', 'F011', 'M017', 'M004', 'M001', 'F012', 'F010', 'F013', 'F001', 'M008', 'F023', 'F020', 'M015', 'M012', 'M003', 'M010', 'F022', 'F006', 'M018', 'F005', 'F021', 'F019']
+
+# 3-fold: 2-fold for training and 1-fold for testing
+SUBJECTS_1 = ['M016', 'M015', 'F020', 'M003', 'F019', 'F021', 'F004', 'M017', 'M011', 'M006', 'M004', 'M014', 'F016', 'F008']
+SUBJECTS_2 = ['F017', 'F012', 'F011', 'M001', 'M002', 'F013', 'M018', 'F015', 'F002', 'F018', 'M010', 'M012', 'M009', 'F001']
+SUBJECTS_3 = ['M007', 'F022', 'M008', 'F003', 'F014', 'F006', 'M005', 'F023', 'F009', 'F005', 'F007', 'M013', 'F010']
+
+
+def generate_3_fold_subjects():
+    random.shuffle(BP4D_SUBJECTS)
+
+    # Split the list into 3 sub-lists
+    sublist1 = random.sample(BP4D_SUBJECTS, 14)
+    for element in sublist1:
+        BP4D_SUBJECTS.remove(element)
+
+    sublist2 = random.sample(BP4D_SUBJECTS, 14)
+    for element in sublist2:
+        BP4D_SUBJECTS.remove(element)
+
+    sublist3 = BP4D_SUBJECTS
+
+    # Print the sub-lists
+    print("Sublist 1:", sublist1)
+    print("Sublist 2:", sublist2)
+    print("Sublist 3:", sublist3)
+
+def split_BP4D_train_test(json_file=None, train_output_file=None, test_output_file=None, test_subjects=None):
+    """
+    """
+
+    with open(json_file, 'r') as file:
+        train_samples = []
+        test_samples = []
+        train_counter = 0
+        test_counter = 0
+
+        # each line is a sample
+        for line in file:
+            loaded_dict = json.loads(line)
+            subject = str(loaded_dict['img_path'].split('/')[0])
+
+            if subject in test_subjects:
+                test_samples.append(loaded_dict)
+                test_counter += 1
+            else:
+                train_samples.append(loaded_dict)
+                train_counter += 1
+
+    # Write training samples to a new JSON file
+    with open(train_output_file, 'w') as f:
+        for sample in train_samples:
+            json.dump(sample, f)
+            f.write('\n')
+    print(f"{train_counter} samples has been written into {train_output_file}")
+
+    # Write test samples to a new JSON file
+    with open(test_output_file, 'w') as f:
+        for sample in test_samples:
+            json.dump(sample, f)
+            f.write('\n')
+    print(f"{test_counter} samples has been written into {train_output_file}")
+
+
+if __name__ == "__main__":
+    # generate_3_fold_subjects()
+    split_BP4D_train_test(json_file='./BP4D_labels.json',
+                          train_output_file='./BP4D_train3.json',
+                          test_output_file='./BP4D_test3.json',
+                          test_subjects=SUBJECTS_3)
--- engine_finetune.py
+++ engine_finetune.py
@@ -12,6 +12,9 @@
 import math
 import sys
 from typing import Iterable, Optional
+import numpy as np
+
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score, average_precision_score
 
 import torch
 
@@ -31,17 +34,14 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     metric_logger = misc.MetricLogger(delimiter="  ")
     metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
     header = 'Epoch: [{}]'.format(epoch)
-    print_freq = 20
-
+    print_freq = 100  # print log every 20 steps
     accum_iter = args.accum_iter
-
     optimizer.zero_grad()
 
     if log_writer is not None:
         print('log_dir: {}'.format(log_writer.log_dir))
 
     for data_iter_step, (samples, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-
         # we use a per iteration (instead of per epoch) lr scheduler
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
@@ -55,9 +55,9 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         with torch.cuda.amp.autocast():
             outputs = model(samples)
             loss = criterion(outputs, targets)
-
         loss_value = loss.item()
 
+        # handle nan loss
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
             sys.exit(1)
@@ -66,9 +66,10 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         loss_scaler(loss, optimizer, clip_grad=max_norm,
                     parameters=model.parameters(), create_graph=False,
                     update_grad=(data_iter_step + 1) % accum_iter == 0)
+
+        # update model parameters
         if (data_iter_step + 1) % accum_iter == 0:
             optimizer.zero_grad()
-
         torch.cuda.synchronize()
 
         metric_logger.update(loss=loss_value)
@@ -77,15 +78,15 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         for group in optimizer.param_groups:
             min_lr = min(min_lr, group["lr"])
             max_lr = max(max_lr, group["lr"])
-
         metric_logger.update(lr=max_lr)
 
+        # log into tensorboard
         loss_value_reduce = misc.all_reduce_mean(loss_value)
         if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
-            """ We use epoch_1000x as the x-axis in tensorboard.
+            """ We use epoch_100x as the x-axis in tensorboard.
             This calibrates different curves when batch size changes.
             """
-            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 100)
             log_writer.add_scalar('loss', loss_value_reduce, epoch_1000x)
             log_writer.add_scalar('lr', max_lr, epoch_1000x)
 
@@ -126,5 +127,88 @@ def evaluate(data_loader, model, device):
     metric_logger.synchronize_between_processes()
     print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
           .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
+
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+@torch.no_grad()
+def AU_evaluate(data_loader, model, device):
+    criterion = torch.nn.BCEWithLogitsLoss()
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    header = 'Test:'
+    model.eval()  # switch to evaluation mode
+    all_preds = []
+    all_targets = []
+
+    for batch in metric_logger.log_every(data_loader, 100, header):
+        images = batch[0]
+        target = batch[-1]
+        images = images.to(device, non_blocking=True)
+        target = target.to(device, non_blocking=True)  # 2D tensor (batch, 12)
+
+        # compute loss
+        with torch.cuda.amp.autocast():
+            output = model(images)  # 2D tensor (batch, 12)
+            loss = criterion(output, target)
+        metric_logger.update(loss=loss.item())
+
+        # for f1 computation
+        sigmoid = torch.nn.Sigmoid()
+        probs = sigmoid(output)
+        all_preds.append(probs.detach().cpu().numpy())
+        all_targets.append(target.detach().cpu().numpy())
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print('* loss {losses.global_avg:.3f}'.format(losses=metric_logger.loss))
+
+    # compute f1
+    y_probs = np.concatenate([arr for arr in all_preds], axis=0)
+    y_true = np.concatenate([arr for arr in all_targets], axis=0)
+
+    # AP = []
+    # for cls in range(y_true.shape[1]):
+    #     AP.append(average_precision_score(y_true[:, cls], y_probs[:, cls]))
+    # # print(f"AP for each category: {AP}")
+    # mAP = np.mean(AP)
+
+    # each AU uses a different threshold
+    # f1_score_ls = []
+    # for i in range(1, 100):
+    #     threshold = i * 0.01
+    #     y_pred = np.zeros(y_probs.shape)
+    #     y_pred[np.where(y_probs >= threshold)] = 1
+    #
+    #     # Compute F1 score for each class
+    #     f1_scores = []
+    #     for class_idx in range(y_true.shape[1]):
+    #         f1_scores.append(f1_score(y_true[:, class_idx], y_pred[:, class_idx]))
+    #     f1_score_ls.append(f1_scores)
+    #
+    #     # compute accuracy
+    #     roc_auc = roc_auc_score(y_true, y_pred, average='micro')
+    #     accuracy = accuracy_score(y_true, y_pred)
+    #
+    # f1_score_arr = np.array(f1_score_ls)
+    # avg_best_f1 = np.max(f1_score_arr, axis=0)
+    # print(f"f1_mean: {avg_best_f1.mean()}, best_f1_scores: {avg_best_f1}")
+
+    # all AUs use the same threshold
+    f1_score_ls = []
+    for i in range(1, 100):
+        threshold = i * 0.01
+        y_pred = np.zeros(y_probs.shape)
+        y_pred[np.where(y_probs >= threshold)] = 1
+
+        # Compute F1 score for each class
+        f1_scores = []
+        for class_idx in range(y_true.shape[1]):
+            f1_scores.append(f1_score(y_true[:, class_idx], y_pred[:, class_idx]))
+        f1_score_ls.append(f1_scores)
+
+    f1_score_arr = np.array(f1_score_ls)
+    max_f1_row_index = np.argmax(np.mean(f1_score_arr, axis=1))
+    max_mean_row = f1_score_arr[max_f1_row_index]
+    print(f"f1_mean: {max_mean_row.mean()} with threshold {(max_f1_row_index+1)/100}, best_f1_scores: {max_mean_row}")
 
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
