8650da68f670cade7fd65bb3ccd65ec11c9aca6f
forever208 <ningmang666@gmail.com>
2024-01-20 21:04:33 +0100

    multi GPU pre-training on SURF

--- main_pretrain.py
+++ main_pretrain.py
@@ -141,7 +141,7 @@ def main(args):
     # define the model
     model = models_mae.__dict__[args.model](norm_pix_loss=args.norm_pix_loss)
     model.to(device)
-    summary(model, input_size=(3, args.input_size, args.input_size), batch_size=-1)
+    # summary(model, input_size=(3, args.input_size, args.input_size), batch_size=-1)
     model_without_ddp = model
 
     eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
@@ -178,8 +178,8 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        # save ckpt for every 5 epoch
-        if args.output_dir and (epoch % 5 == 0 or epoch + 1 == args.epochs):
+        # save ckpt for every 1 epoch
+        if args.output_dir and (epoch % 1 == 0 or epoch + 1 == args.epochs):
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch
--- submitit_pretrain.py
+++ submitit_pretrain.py
@@ -19,12 +19,12 @@ import submitit
 def parse_args():
     trainer_parser = trainer.get_args_parser()
     parser = argparse.ArgumentParser("Submitit for MAE pretrain", parents=[trainer_parser])
-    parser.add_argument("--ngpus", default=8, type=int, help="Number of gpus to request on each node")
-    parser.add_argument("--nodes", default=2, type=int, help="Number of nodes to request")
+    parser.add_argument("--ngpus", default=4, type=int, help="Number of gpus to request on each node")
+    parser.add_argument("--nodes", default=1, type=int, help="Number of nodes to request")
     parser.add_argument("--timeout", default=4320, type=int, help="Duration of the job")
     parser.add_argument("--job_dir", default="", type=str, help="Job dir. Leave empty for automatic.")
 
-    parser.add_argument("--partition", default="learnfair", type=str, help="Partition where to submit")
+    parser.add_argument("--partition", default="gpu", type=str, help="Partition where to submit")
     parser.add_argument("--use_volta32", action='store_true', help="Request 32G V100 GPUs")
     parser.add_argument('--comment', default="", type=str, help="Comment to pass to scheduler")
     return parser.parse_args()
@@ -32,8 +32,8 @@ def parse_args():
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/experiments")
+    if Path("checkpoint/").is_dir():
+        p = Path(f"/home/mning/mae/checkpoint/{user}/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
@@ -106,7 +106,7 @@ def main():
         mem_gb=40 * num_gpus_per_node,
         gpus_per_node=num_gpus_per_node,
         tasks_per_node=num_gpus_per_node,  # one task per GPU
-        cpus_per_task=10,
+        cpus_per_task=18,
         nodes=nodes,
         timeout_min=timeout_min,  # max is 60 * 72
         # Below are cluster dependent parameters
