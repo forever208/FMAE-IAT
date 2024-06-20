d89907c4d31584ed20837784627218763b044ccf
forever208 <ningmang666@gmail.com>
2024-06-20 16:27:49 +0200

    add BP4D+ finetune, modify its 'dataset' class

--- models_vit.py
+++ models_vit.py
@@ -31,7 +31,7 @@ class GradReverse(Function):
 class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
     """ Vision Transformer with support for global average pooling
     """
-    def __init__(self, global_pool=False, grad_reverse=None, **kwargs):
+    def __init__(self, global_pool=False, grad_reverse=0, num_classes=0, num_subjects=0, **kwargs):
         super(VisionTransformer, self).__init__(**kwargs)
 
         self.global_pool = global_pool
@@ -43,11 +43,12 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
             del self.norm  # remove the original norm
 
         # Classifier head
-        self.AU_head = nn.Linear(kwargs['embed_dim'], 12)
+        self.AU_head = nn.Linear(kwargs['embed_dim'], num_classes)
         self.grad_reverse = grad_reverse  # default is 1.0
         print(f"using ID adversarial: {self.grad_reverse}" )
+        print(f"num classes: {num_classes}, num subjects: {num_subjects}")
         if not self.grad_reverse == 0:
-            self.ID_head = nn.Linear(kwargs['embed_dim'], 41)
+            self.ID_head = nn.Linear(kwargs['embed_dim'], num_subjects)
             print(f"activate ID adver head")
 
 
